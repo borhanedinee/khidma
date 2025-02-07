3d1f99831f25678b56536eb7d2e6d1c05facda1f
Borhan-svg <63272347+Borhan-svg@users.noreply.github.com>
2025-02-07 16:51:14 +0100

    deployment

--- frontend/lib/constatnts/constants.dart
+++ frontend/lib/constatnts/constants.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: constant_identifier_names
+const String URL = "https://khidma-borhanedinee-dev.apps.rm3.7wse.p1.openshiftapps.com";
 
-const String BASE_URL = "http://10.0.2.2:3000";
-const String IMAGE_URL = "http://10.0.2.2:3000/uploads/logos";
-const String RESUME_URL = "http://10.0.2.2:3000/uploads/cvs";
+const String BASE_URL = URL;
+const String IMAGE_URL = "$URL/uploads/logos";
+const String RESUME_URL = "$URL/uploads/cvs";
 
 const Map<String, String> HEADERS = {
   'Content-Type': 'application/json',
--- khidma-backend/project/config/db.js
+++ khidma-backend/project/config/db.js
@@ -1,16 +1,39 @@
+// const mysql = require('mysql');
+// require('dotenv').config();
+
+// const db = mysql.createConnection({
+//     // host: process.env.DB_HOST,
+//     // user: process.env.DB_USER,
+//     // password: process.env.DB_PASS,
+//     // database: process.env.DB_NAME,
+    
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'khidma',
+// });
+
+// module.exports = db;
+
+
 const mysql = require('mysql');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-    
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'khidma',
+    host: process.env.DB_HOST || '172.30.179.233',  // OpenShift MySQL Host
+    port: process.env.DB_PORT || '3306',  // MySQL Port
+    user: process.env.DB_USER || 'myuser',  // Database Username
+    password: process.env.DB_PASS || 'mypassword',  // Database Password
+    database: process.env.DB_NAME || 'khidma',  // Database Name
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('❌ Database connection failed:', err);
+        process.exit(1); // Exit if the connection fails
+    } else {
+        console.log('✅ Connected to the database');
+    }
 });
 
 module.exports = db;
